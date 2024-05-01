fbc2fc0dc783442ae574eef863b44c3aa09c63ed
arano1212 <arano1212@gmail.com>
2024-05-01 14:54:10 -0600

    rutas corregidas

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,14 +2,12 @@ import express from 'express'
 import { createMovie, deleteMovieById, getAllMovies, getMovieById, updateMovieById } from '../controller/movieController.js'
 import { isAuth } from '../middlewares/isAuth.js'
 import { isAdmin } from '../middlewares/isAdmin.js'
-import { isEmployee } from '../middlewares/isEmployee.js'
-import { isCustomer } from '../middlewares/isCustomer.js'
 
 const movieRoutes = express.Router()
 
 movieRoutes.post('/', isAuth, isAdmin, createMovie)
-movieRoutes.get('/', isAuth, isAdmin, isEmployee, isCustomer, getAllMovies)
-movieRoutes.get('/:movieId', isAuth, isAdmin, isCustomer, isEmployee, getMovieById)
+movieRoutes.get('/', isAuth, getAllMovies)
+movieRoutes.get('/:movieId', isAuth, getMovieById)
 movieRoutes.patch('/:movieId', isAuth, isAdmin, updateMovieById)
 movieRoutes.delete('/:movieId', isAuth, isAdmin, deleteMovieById)
 
--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -3,7 +3,6 @@ import { isAuth } from '../middlewares/isAuth.js'
 import { isAdmin } from '../middlewares/isAdmin.js'
 import { DeleteTicketCustomer, createTicketAdminAndEmployee, createTicketCustomer, getAllTickets, getTicketById, getTicketByIdCustomer, updateTicketById } from '../controller/ticketController.js'
 import { isCustomer } from '../middlewares/isCustomer.js'
-import { isEmployee } from '../middlewares/isEmployee.js'
 
 const ticketRoutes = express.Router()
 
@@ -11,7 +10,7 @@ ticketRoutes.post('/', isAuth, isAdmin, createTicketAdminAndEmployee)
 ticketRoutes.post('/customers', isAuth, isCustomer, createTicketCustomer)
 ticketRoutes.get('/', isAuth, isAdmin, getAllTickets)
 ticketRoutes.get('/:ticketId', isAuth, isAdmin, getTicketById)
-ticketRoutes.get('/customers/:ticketCustomerId', isAuth, isAdmin, isCustomer, isEmployee, getTicketByIdCustomer)
+ticketRoutes.get('/customers/:ticketCustomerId', isAuth, getTicketByIdCustomer)
 ticketRoutes.patch('/customers/:ticketCustomerId', isAuth, isAdmin, updateTicketById)
 ticketRoutes.delete('/customers/:ticketCustomerId', isAuth, isAdmin, DeleteTicketCustomer)
 
